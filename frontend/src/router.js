29bb79fd771f122a919ab7c16161e95265505762
David Tovmasyan <dtovmasyan2022@gmail.com>
2024-04-15 21:57:29 +0200

    Note page almost works, but only localy

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "./routes/App";
 import Login from "./routes/Login";
 import Signup from "./routes/Signup";
 
